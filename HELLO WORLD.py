a4ea709103dda156694fe9a62c655e155a0f6d0f
Hemanth Parthasarathy <65104444+HemanthParthasarathy@users.noreply.github.com>
2020-12-14 11:45:10 +0530

    Add files via upload

---
+++ HELLO WORLD.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pip install pandas
+
+
+# In[2]:
+
+
+print ("HELLO WORLD!!THIS MARKS A NEW BEGINNING")
+
+
+# In[7]:
+
+
+import colorama
+from colorama import Fore,Back
+print (Fore.YELLOW + Back.BLACK + "THIS TEXT WILL BE IN TOTAL NEW FOREGROUND AND BACKGROUND COLOUR...")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
